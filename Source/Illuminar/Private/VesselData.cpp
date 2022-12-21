a57c6d77fd8c7c29eaf2da32885b6d0f85ffe5f0
Abhishek Rakshit <abhishek0616@gmail.com>
2022-12-20 16:29:46 -0800

    Added functionality to change vessel properties based on the honesty input

---
+++ Source/Illuminar/Private/VesselData.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VesselData.h"
+string name;
+string description;
+
+VesselData::VesselData(name, description)
+{
+    this.name = name;
+    this.description = description;
+}
+
+VesselData::~VesselData()
+{
+}
